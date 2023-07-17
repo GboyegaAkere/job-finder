d456bee8d890655c91d48bc636d2be860833d563
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2023-07-17 12:31:47 -0700

    new commit

--- find/App.js
+++ find/App.js
@@ -1,16 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import AppNavigator from './src/AppNavigator';
+
 
 export default function App() {
   return (
-
-   <View className="flex-1 items-center justify-center">
-      <View className ="bg-purple-700 px-6 rounded-2xl">
-        <Text className="text-lg">Open App</Text>
-        <StatusBar style="auto" />
-      </View>
-    </View>
-  );
+       <AppNavigator/>
+      );
 }
 
